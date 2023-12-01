e16507cfcb08e88c3885bd6eb8cc43bb1b3105ac
aryan <notlelouchvb@gmail.com>
2023-12-01 13:33:03 +0530

    added transaction

---
+++ transaction.js
@@ -0,0 +1,25 @@
+class Transaction {
+    constructor(opts={}) {
+        Object.assign(this, {
+            from: null, to: null, value: 0, nounce: 0, signature: null
+        }, opts);
+    }
+
+    hash() {
+        const head = JSON.stringify(this, ['from', 'to', 'value', 'nounce']);
+        return createHash('SHA256').update(head).digest('hex');
+    }
+
+    sign(privateKey) {
+        const cert = Wallet.getNodePrivateKey(this.from, privateKey);
+        const signature = createSign('SHA256').update(this.hash()).sign(cert, 'hex');
+        return new Transaction({...this, signature});
+    }
+
+    test() {
+        const cert = Wallet.getNodePublicKey(this.from);
+        const signature = createVerify('SHA256').update(this.hash());
+        if (!(this.from) || !(this.signature)) return false;
+        return signature.verify(cert, this.signature, 'hex');
+    }
+}
